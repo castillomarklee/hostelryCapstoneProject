dd6f1c48fb6c1c37026ebaeec381591fccf91515
castilloleem <castillolee@yahoo.com>
2018-03-04 21:19:15 -0800

    minor changes

--- services/confirmprint.php
+++ services/confirmprint.php
@@ -5,7 +5,7 @@
 	session_start();
 	$hid = $_SESSION['hostelrylogin'];
 
-	$query = mysqli_query($conn, "SELECT q.firstname, q.middlename, q.lastname FROM customer q, reservation w, rooms e, hostelryacc r WHERE q.customer_id=w.customer_id AND e.room_id=w.room_id AND r.hostelry_id=e.hostelry_id AND r.hostelry_id='$hid' AND w.reservation_status='confirmed'");
+	$query = mysqli_query($conn, "SELECT q.firstname, q.middlename, q.lastname, e.room_name FROM customer q, reservation w, rooms e, hostelryacc r WHERE q.customer_id=w.customer_id AND e.room_id=w.room_id AND r.hostelry_id=e.hostelry_id AND r.hostelry_id='$hid' AND w.reservation_status='confirmed'");
 
 	header("Content-type: application/vnd.ms-word");
 	header("Content-Disposition: attachment;Filename=confirmed_list.doc");
@@ -17,7 +17,8 @@
 	while ($row = mysqli_fetch_assoc($query)) {
 		echo "<h5>Firstname: " . $row['firstname']. "</h5>";
 		echo "<h5>Middlename: " . $row['middlename']. "</h5>";
-		echo "<h5>Lastname: " . $row['lastname'] . "</h5><br><br>";
+		echo "<h5>Lastname: " . $row['lastname'] . "</h5>";
+		echo "<h5>Room: " . $row['room_name'] . "</h5><br><br>";
 	}
 	echo "</body>";
 	echo "</html>";
